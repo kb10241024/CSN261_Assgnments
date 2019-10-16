a85b4fa5c851a73e79ff29feb25a41ee7ed90319
kb10241024 <kavyabarnwal1@gmail.com>
2019-10-16 12:39:37 +0530

    updated

--- ass-7/q1/asn7/Point.java
+++ ass-7/q1/asn7/Point.java
@@ -1,25 +1,24 @@
 /**
  * @file Point.java
  * @date 11/10/2019
- * @author Ritik Jain
+ * @author Kavya Barnwal
  * @brief Implements a K-Dimensional Point
 */
- 
+
 package asn7;
 
 public class Point<T> {
-    
+
     public T[] point;
     public int K;
-    
+
     /**
-     * @author Ritik Jain
+     * @author Kavya Barnwal
      * @date 11/10/2019
      * @brief Initializes the tree
-     * @param point  The point
+     * @param point The point
      */
-    public Point(T[] point)
-    {
+    public Point(T[] point) {
         this.point = point;
         K = point.length;
     }
